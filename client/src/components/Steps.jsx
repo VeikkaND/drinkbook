82be36657731fb3c13174aa57392a05b62e79860
Veikka <veikka.nevala@outlook.com>
2025-06-06 16:28:34 +0300

    only create steps if steps exist

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,12 +1,14 @@
 function Steps({text}) {
-    console.log(text)
-    const steps = text.split("|") //create array of steps
-    return(
-        <div>
-            {steps.map((step, i) => 
-            <p key={i}>{step}</p>)}
-        </div>
+    if(text) {
+        const steps = text.split("|") //create array of steps
+        return(
+            <div>
+                {steps.map((step, i) => 
+                <p key={i}>{step}</p>)}
+            </div>
     )
+    }
+    
 }
 
 export default Steps
