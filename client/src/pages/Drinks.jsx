b56ecb942d0cf40e1b995d7c66c55f75a7c16b7b
Veikka <veikka.nevala@outlook.com>
2025-06-14 15:00:13 +0300

    sorting in /drinks/

--- client/src/pages/Drinks.jsx
+++ client/src/pages/Drinks.jsx
@@ -1,19 +1,52 @@
 import { useEffect } from "react"
 import drinkService from "../services/drink"
 import { useState } from "react"
-import { NavLink } from "react-router"
 import DrinkLink from "../components/DrinkLink"
 import SearchInput from "../components/SearchInput"
 
 function Drinks() {
     const [drinks, setDrinks] = useState([])
+    const [sort, setSorting] = useState("stars")
     useEffect(() => {
         const getDrinks = async () => {
             const res = await drinkService.getAllDrinks()
             setDrinks(res)
+            sortDrinks()
         }
         getDrinks()
     }, [])
+
+    const handleChange = (event) => {
+        const val = event.target.value
+        switch(val) {
+            case "stars":
+                setSorting("stars")
+                break
+            case "alphabetical":
+                setSorting("alphabetical")
+                break
+        }
+    }
+
+    const sortDrinks = () => {
+        const array = drinks
+        switch(sort) {
+            case "stars":
+                array.sort((a,b) => b.stars - a.stars)
+                return array.map((drink) => 
+                    <DrinkLink key={drink.drink_id} drink={drink}/>)
+            case "alphabetical":
+                array.sort((a,b) => {
+                    const aName = a.name.toLowerCase()
+                    const bName = b.name.toLowerCase()
+                    if(aName < bName) {return -1}
+                    if(aName > bName) {return 1}
+                    return 0
+                })
+                return array.map((drink) => 
+                    <DrinkLink key={drink.drink_id} drink={drink}/>)
+        }
+    }
 
     return(
         <div>
@@ -21,7 +54,7 @@ function Drinks() {
             <SearchInput />
             <label>
                 sort by:
-                <select>
+                <select onChange={handleChange}>
                     <option value={"stars"}>
                         Stars</option>
                     <option value={"alphabetical"}>
@@ -30,8 +63,7 @@ function Drinks() {
             </label>
             
             <div className="drinks">
-                {drinks.map((drink) => 
-                    <DrinkLink key={drink.drink_id} drink={drink}/>)}
+                {sortDrinks()}
             </div>
             
         </div>
